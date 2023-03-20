caab44b47de5220b5b35d91c06b2561329e606a5
SirShagalot <56863638+SirShagalot@users.noreply.github.com>
2023-03-20 21:05:05 +0100

    næsten færdig

--- app.js
+++ app.js
@@ -38,6 +38,30 @@ function initApp() {
     potterActor,
     potterImage
   );
+
+  showCharacter(
+    "Ron Weasley",
+    "male",
+    "Gryffindor",
+    "01-03-1980",
+    "pure blood",
+    "blue",
+    "red",
+    "Rupert Grint",
+    "http://hp-api.herokuapp.com/images/ron.jpg"
+  );
+
+  showCharacter(
+    potterName,
+    potterGender,
+    potterHouse,
+    potterDateOfBirth,
+    potterAncestry,
+    potterEyeColour,
+    potterHairColour,
+    potterActor,
+    potterImage
+  );
 }
 
 function showCharacter(
@@ -51,50 +75,113 @@ function showCharacter(
   actor,
   image
 ) {
-  //   const newParagraph = document.createElement("p");
-  //   newParagraph.textContent = "My new paragraph";
-  //   document.querySelector("body").appendChild(newParagraph);
-  const articleElement = document.createElement("article");
-  const nameElement = document.createElement("h2");
-  const genderElement = document.createElement("p");
-  const dateElement = document.createElement("p");
-  const houseElement = document.createElement("p");
-  const ancestryElement = document.createElement("p");
-  const eyeColourElement = document.createElement("p");
-  const hairColourElement = document.createElement("p");
-  const actorElement = document.createElement("p");
-  const imageElement = document.createElement("img");
+  const myHTML =
+    /*html*/
+    `<article>
+    <img src=${image}>
+    <h2>${name}</h2>
+    <p>${house}</p>
+    <P>${ancestry}</P>
+    <p>Date of Birth: ${dateOfBirth}</p>
+    <p>Eye colour: ${eyeColour}</p>
+    <p>Hair colour: ${hairColour}</p>
+    <p>${name} is played by ${actor}</p>
+    </article>`;
 
-  nameElement.textContent = name;
-  genderElement.textContent = gender;
-  houseElement.textContent = house;
-  ancestryElement.textContent = ancestry;
-  eyeColourElement.textContent = eyeColour;
-  hairColourElement.textContent = eyeColour;
-  dateElement.textContent = `Date of Birth: ${dateOfBirth}`;
-  actorElement.textContent = `${name} is played by ${actor}`;
-  imageElement.src = image;
-  console.log(actorElement);
+  // document.querySelector("#characters").insertAdjecentHTML(beforeedn, myHTML);
 
-  articleElement.appendChild(imageElement);
-  articleElement.appendChild(nameElement);
-  articleElement.appendChild(genderElement);
-  articleElement.appendChild(houseElement);
-  articleElement.appendChild(dateElement);
-  articleElement.appendChild(ancestryElement);
-  articleElement.appendChild(eyeColourElement);
-  articleElement.appendChild(hairColourElement);
-  articleElement.appendChild(actorElement);
+  const genderCapped = capitalize(gender);
+  const newBirthdate = changeFormat(dateOfBirth);
+  const banner = coatOfArms(house);
 
-  console.log(
-    name,
-    gender,
-    house,
-    dateOfBirth,
-    ancestry,
-    eyeColour,
-    hairColour,
-    actor,
-    image
-  );
+  const trElement = /*html*/ `
+  <tr>
+  <td><img src=${image}></td>
+  <td>${name}</td>
+  <td><img src=${house}></td>
+  <td> ${genderCapped} </td>
+  <td>${newBirthdate}</td>
+  </tr>
+  `;
+
+  document
+    .querySelector("#characterTables")
+    .insertAdjacentHTML("beforeend", trElement);
+}
+
+function capitalize(word) {
+  const newWord = word[0].toUpperCase() + word.substring(1);
+  return newWord;
+}
+
+function changeFormat(date) {
+  const firstSign = date.indexOf("-");
+  const day = date.substring(0, firstSign);
+  const lastSign = date.lastIndexOf("-");
+  const month = date.substring(firstSign + 1, lastSign);
+  const year = date.substring(lastSign + 1);
+  // const newdate = date.replace("-", " ");
+  return `${day}/${month}/${year}`;
+}
+
+function coatOfArms(house) {
+  let banner;
+  if (house == "Gryffindor") {
+    banner =
+      "https://cederdorff.github.io/dat-js/05-data/peters-potter-app/images/housecrest-gryffindor.png";
+  } else if (house == "Slytherin") {
+    banner =
+      "https://cederdorff.github.io/dat-js/05-data/peters-potter-app/images/housecrest-slytherin.png";
+  } else if (house == "Racenclaw") {
+    banner =
+      "https://cederdorff.github.io/dat-js/05-data/peters-potter-app/images/housecrest-ravenclaw.png";
+  } else {
+    banner =
+      "https://cederdorff.github.io/dat-js/05-data/peters-potter-app/images/housecrest-hufflepuff.png";
+  }
+  return banner;
+}
+
+function originalShowCharacter(
+  name,
+  gender,
+  house,
+  dateOfBirth,
+  ancestry,
+  eyeColour,
+  hairColour,
+  actor,
+  image
+) {
+  // const articleElement = document.createElement("article");
+  // const nameElement = document.createElement("h2");
+  // const genderElement = document.createElement("p");
+  // const dateElement = document.createElement("p");
+  // const houseElement = document.createElement("p");
+  // const ancestryElement = document.createElement("p");
+  // const eyeColourElement = document.createElement("p");
+  // const hairColourElement = document.createElement("p");
+  // const actorElement = document.createElement("p");
+  // const imageElement = document.createElement("img");
+  // nameElement.textContent = name;
+  // genderElement.textContent = gender;
+  // houseElement.textContent = house;
+  // ancestryElement.textContent = ancestry;
+  // eyeColourElement.textContent = `Eye colour: ${eyeColour}`;
+  // hairColourElement.textContent = `Hair colour: ${hairColour}`;
+  // dateElement.textContent = `Date of Birth: ${dateOfBirth}`;
+  // actorElement.textContent = `${name} is played by ${actor}`;
+  // imageElement.src = image;
+  // console.log(actorElement);
+  // articleElement.appendChild(imageElement);
+  // articleElement.appendChild(nameElement);
+  // articleElement.appendChild(genderElement);
+  // articleElement.appendChild(houseElement);
+  // articleElement.appendChild(dateElement);
+  // articleElement.appendChild(ancestryElement);
+  // articleElement.appendChild(eyeColourElement);
+  // articleElement.appendChild(hairColourElement);
+  // articleElement.appendChild(actorElement);
+  // document.querySelector("#characters").appendChild(articleElement);
+  // console.log(articleElement);
 }
